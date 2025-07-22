4f85fd4b9799c6d105001d6e922a38d5496348d9
Ahsan-Yasin <f233063@cfd.nu.edu.pk>
2025-07-22 18:09:58 +0000

    files

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,13 +1,14 @@
 import numpy as np 
 import pandas as pd  
 
-
+#this function will fetch data 
 def get_data(file_name): 
     try: 
         df=pd.read_csv(file_name)
         return df 
     except Exception as e:
         print('ERROR WHILE READING FILE ')
-        raise(e)
-df=get_data('raw_data/personality_dataset.csv') 
-df.head()
+        raise(e) 
+if __name__=="__main__":
+    df=get_data('raw_data/personality_dataset.csv') 
+    print(df.head())
---
+++ src/data_processing.py
@@ -0,0 +1,25 @@
+#dataprocessing.py file  
+import pandas as pd 
+import numpy as np   
+import os  
+from data_ingestion import get_data
+def process_personality_dataset(df: pd.DataFrame) -> pd.DataFrame: 
+    df['Stage_fear']= df['Stage_fear'].map({"Yes":1,"No":0}) 
+    df['Drained_after_socializing']=df['Drained_after_socializing'].map({"Yes":1,"No":0}) 
+    df['Personality']=df['Personality'].map({"Extrovert":1,"Introvert":0})
+    save_data(df)
+    print(df.head())
+    return df 
+
+def save_data(df: pd.DataFrame) ->None:  
+    try:
+        folder_path = "data/processed"
+        os.makedirs(folder_path, exist_ok=True)
+        file_path = os.path.join(folder_path, "processed_personality.csv")
+        df.to_csv(file_path, index=False)
+    except Exception as e: 
+        print("ERROR WHILE CREATING DATA FOLDER WITH PROCESSED CSV ")
+        raise(e) 
+
+df=get_data('raw_data/personality_dataset.csv')
+process_personality_dataset(df)
