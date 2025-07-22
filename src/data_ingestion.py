5590409af9ac61d8d0e07013fe75c260f1a52084
Ahsan-Yasin <f233063@cfd.nu.edu.pk>
2025-07-22 17:27:02 +0000

    bug

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd  
 
 
-def get_data(name): 
+def get_data(file_name): 
     try: 
-        df=pd.read_csv('name')
+        df=pd.read_csv(file_name)
         return df 
     except Exception as e:
         print('ERROR WHILE READING FILE ')
